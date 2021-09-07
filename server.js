2ebcf8df654c17f206a927ccf7eda81252ef16c0
Nam Phạm <namcyeon@gmail.com>
2021-09-07 15:22:25 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,9 +9,14 @@ const port = process.env.PORT || 3030;
 ////////////////
 // MIDDLEWARE //
 ////////////////
+const corsOptions ={
+   origin:'*', 
+   credentials:true,
+   optionSuccessStatus:200,
+}
 
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOptions))
 app.use((req, res, next) => {
 	console.log('MIDDLEWARE LOG', req.body)
 	next()
